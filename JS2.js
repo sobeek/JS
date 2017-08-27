7239ee0ff977bbae510a8025fce43c5b5ffa0807
Pavel Sobyanin <sobeek@yandex.ru>
2017-08-27 17:28:07 +0300

    first commit

---
+++ JS2.js
@@ -0,0 +1,38 @@
+function matrix_spiral(matrix) {
+
+    var dim = matrix.length;
+
+    var i = Math.floor (dim / 2);
+    var j = i;
+    var r = i;
+    var l = i;
+
+    for (var n = 0; n < dim * dim; ++n)
+    {
+        console.log(matrix[i][j]);
+
+        if ((i == l) && (j == l)) {r++; l--; j--; continue} // go to external spiral
+        if ((i == l) && (j > l)) {j--; continue} //go left
+        if ((i < r) && (j == l)) {i++; continue} //down
+        if ((i == r) && (j < r)) {j++; continue} //right
+        if ((i > l) && (j == r)) {i--; continue} //up
+    }
+}
+
+var matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+matrix = [[100]];
+
+matrix = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11,12,13,14,15],
+    [16,17,18,19,20],
+    [21,22,23,24,25]
+];
+
+matrix_spiral(matrix);
