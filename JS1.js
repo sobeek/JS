62bcbcdfbfbb15d9d78d8e4cc9d5301bf3a0b626
Pavel Sobyanin <sobeek@yandex.ru>
2017-08-27 17:38:01 +0300

    spiral

---
+++ JS1.js
@@ -0,0 +1,15 @@
+function isInteger() {
+    //var rounded = num ^ 0;
+    //return num == (num ^ 0)
+    var myName = prompt("Enter your name", "");// || "Nameless";
+    //if (myName === "") {myName += "Nameless"};
+    alert(typeof myName);
+    alert(myName);
+}
+
+//isInteger();
+
+//var x = 2 ** 33;
+//alert(x^0)
+
+alert( NaN == NaN )
--- matrix_spiral.js
+++ matrix_spiral.js
@@ -1,15 +1,39 @@
-function isInteger() {
-    //var rounded = num ^ 0;
-    //return num == (num ^ 0)
-    var myName = prompt("Enter your name", "");// || "Nameless";
-    //if (myName === "") {myName += "Nameless"};
-    alert(typeof myName);
-    alert(myName);
+function matrix_spiral(matrix) {
+
+    var dim = matrix.length;
+
+    var i = Math.floor (dim / 2);
+    var j = i;
+    var l = i; //left spiral boundary
+    var r = i; // right spiral boundary
+
+
+    for (var n = 0; n < dim * dim; ++n)
+    {
+        console.log(matrix[i][j]);
+
+        if ((i == l) && (j == l)) {r++; l--; j--; continue} // go to external spiral
+        if ((i == l) && (j > l)) {j--; continue} //go left
+        if ((i < r) && (j == l)) {i++; continue} //down
+        if ((i == r) && (j < r)) {j++; continue} //right
+        if ((i > l) && (j == r)) {i--; continue} //up
+    }
 }
 
-//isInteger();
+var matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
 
-//var x = 2 ** 33;
-//alert(x^0)
+matrix = [[100]];
 
-alert( NaN == NaN )
+matrix = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11,12,13,14,15],
+    [16,17,18,19,20],
+    [21,22,23,24,25]
+];
+
+matrix_spiral(matrix);
